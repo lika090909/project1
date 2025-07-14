0fcfcae3f9d93785fe7672937e9ad3b251592d27
lika090909 <likulya157@gmail.com>
2025-07-14 13:30:46 -0700

    buildspec format change

--- part1/p-23-autoscaling-resource.tf
+++ part1/p-23-autoscaling-resource.tf
@@ -1,33 +1,32 @@
-# Autoscaling Group Resource
 resource "aws_autoscaling_group" "my_asg" {
-  name_prefix = "${var.environment}-"
-  desired_capacity = 2
-  max_size = 10
-  min_size = 2
+  name_prefix         = "${var.environment}-"
+  desired_capacity    = 2
+  max_size            = 10
+  min_size            = 2
   vpc_zone_identifier = module.vpc.private_subnets
-  
+
   launch_template {
-  id      = aws_launch_template.my_launch_template.id
-   version = aws_launch_template.my_launch_template.latest_version
+    id      = aws_launch_template.my_launch_template.id
+    version = aws_launch_template.my_launch_template.latest_version
   }
-  
+
   target_group_arns = [module.alb.target_groups[0].arn]
-  
+
   health_check_type = "EC2"
-  #health_check_grace_period = 300 
- 
-     # Instance Refresh
+  # health_check_grace_period = 300
+
   instance_refresh {
     strategy = "Rolling"
     preferences {
       # instance_warmup = 300 # Default behavior is to use the Auto Scaling Groups health check grace period value
-      min_healthy_percentage = 50            
+      min_healthy_percentage = 50
     }
-    triggers = [ "desired_capacity" ] # You can add any argument from ASG here, if those has changes, ASG Instance Refresh will trigger   
+    triggers = ["desired_capacity"] # triggers instance refresh when desired_capacity changes
   }
-   tag {
+
+  tag {
     key                 = "Name"
     value               = "${var.environment}-ecs-instance"
     propagate_at_launch = true
+  }
 }
-  
